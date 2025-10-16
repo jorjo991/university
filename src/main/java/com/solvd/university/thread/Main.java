b91d4b93ff4d8b8f521fb47f346533c8ff29942c
giorgi ugulava <g.ugulava12@gmail.com>
2025-10-16 22:20:16 +0400

    Thread HomeWork

--- src/main/java/com/solvd/university/thread/Main.java
+++ src/main/java/com/solvd/university/thread/Main.java
@@ -1,7 +1,11 @@
 package com.solvd.university.thread;
 
+import java.time.temporal.TemporalUnit;
+import java.util.Scanner;
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadPoolExecutor;
 
 public class Main {
 
@@ -27,5 +31,28 @@ public class Main {
             executorService.execute(task);
         }
         executorService.shutdown();
+
+        CompletableFuture stringCompletableFuture = CompletableFuture.runAsync(() -> {
+            try {
+                Thread.sleep(200);
+                System.out.println("CompletableFuture 1 is running");
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e.getMessage());
+            }
+        });
+
+        try {
+            Thread.sleep(500);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e.getMessage());
+        }
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter two number");
+        int var1 = scanner.nextInt();
+        int var2 = scanner.nextInt();
+        CompletableFuture<Integer> fun1 = CompletableFuture.supplyAsync(() -> var1);
+        CompletableFuture<Integer> fun2 = CompletableFuture.supplyAsync(() -> var2);
+        fun1.thenCombine(fun2, Integer::sum).thenAccept(System.out::println);
+
     }
 }
